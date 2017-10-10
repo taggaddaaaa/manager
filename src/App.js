61478d0b27647d7a379c0f33e02dc3f7ea166807
taggaddaaaa <scaizerg@student.42.fr>
2017-10-10 22:25:54 +0200

    - Build LoginForm and import it in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 
 class app extends Component {
@@ -24,11 +25,7 @@ class app extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)} >
-        <View>
-          <Text>
-             hello!
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
