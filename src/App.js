5b3d5c71d28b612611a1a61e2af5b799c53a1d35
taggaddaaaa <scaizerg@student.42.fr>
2017-10-10 08:30:06 +0200

    -Allow auth with email in firebase console - import firebase inside app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 
 class app extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAz3RaS1MXR0FfZywMvsW1rMM8A59DU6Bw",
+      authDomain: "managemystaff-c0a2b.firebaseapp.com",
+      databaseURL: "https://managemystaff-c0a2b.firebaseio.com",
+      projectId: "managemystaff-c0a2b",
+      storageBucket: "managemystaff-c0a2b.appspot.com",
+      messagingSenderId: "904939818513"
+    };
+
+    firebase.initializeApp(config);
+  }
+
+
   render() {
     return (
       <Provider store={createStore(reducers)} >
