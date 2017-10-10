0c0d4651b70b554cd3c6290f2722e4f770c9cdbe
taggaddaaaa <scaizerg@student.42.fr>
2017-10-10 08:16:35 +0200

    provide reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import react, { component } from 'react';
+import React, { component } from 'react';
 import { view, text } from 'react-native';
 import { provider } from 'react-redux';
 import { createstore } from 'redux';
+import reducers from './reducers';
 
 
 class app extends component {
   render() {
     return (
-      <provider store={createstore()} >
+      <provider store={createstore(reducers)} >
         <view>
           <text>
              hello!
