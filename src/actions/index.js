f370ea3448a159203f222b6e68917bc6ca5b4c28
taggaddaaaa <scaizerg@student.42.fr>
2017-10-11 08:32:36 +0200

    -init state of password

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,20 @@
-import { EMAIL_CHANGED } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from './types';
 
 
 export const emailChanged = (text) => {
   return {
     type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  "use strict";
+  return {
+    type: PASSWORD_CHANGED,
     payload: text
   };
 };
