805da93258b1c9b65fdd06eb0d7f8741ab01dd1e
taggaddaaaa <sabinecaizergues@hotmail.com>
2017-10-11 13:49:27 +0200

    import and use constant

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from './types';
 
 
@@ -24,7 +25,7 @@ export const loginUser = ({ email, password }) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
         dispatch({
-          type: 'LOGIN_USER_SUCCESS',
+          type: LOGIN_USER_SUCCESS,
           payload: user
         });
       });
