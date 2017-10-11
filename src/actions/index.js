581b3873c76b547a30a7e6d90e483bfe50fc8c81
taggaddaaaa <sabinecaizergues@hotmail.com>
2017-10-11 14:10:53 +0200

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from './types';
 
 
@@ -23,11 +24,24 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({
-          type: LOGIN_USER_SUCCESS,
-          payload: user
-        });
+      .then(user => loginUsersuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user));
       });
   };
+};
+
+
+const loginUserFail =(dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
