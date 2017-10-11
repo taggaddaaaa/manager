ca41a37af69721525516d1ac607ae5fe7bc33b63
taggaddaaaa <sabinecaizergues@hotmail.com>
2017-10-11 13:54:30 +0200

    When LOGIN_USER_SUCCESS create a new state with a model user

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,14 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 
@@ -18,6 +20,8 @@ export default (state = INITIAL_STATE, action ) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
