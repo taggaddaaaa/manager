e414b5aa6ad73bce9a40b39b953937bff51d5ffb
taggaddaaaa <sabinecaizergues@hotmail.com>
2017-10-13 21:19:56 +0200

    Login_user

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,13 +2,15 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL
+  LOGIN_USER_FAIL,
+  LOGIN_USER
 } from '../actions/types';
 
 
 const INITIAL_STATE = {
   email: '',
   password: '',
+  loading: false,
   user: null,
   error: ''
 };
@@ -22,6 +24,8 @@ export default (state = INITIAL_STATE, action ) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: ''};
     case LOGIN_USER_SUCCESS:
       return { ...state, user: action.payload, error: '' };
     case LOGIN_USER_FAIL:
